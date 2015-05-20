210669e17b37be76f4c8a057f047286001fbb004
Trey Griffith <trey.griffith@gmail.com>
2015-05-19 20:48:10 -0700

    move json commenting into it's own repo

---
+++ index.js
@@ -0,0 +1,31 @@
+var merge = require('merge'),
+    commentKey = '__comments';
+
+exports.setKey = function (newKey) {
+  commentKey = newKey;
+};
+
+exports.write = function(json, comment) {
+  json = merge(true, json);
+
+  if(!json[commentKey]) {
+    json[commentKey] = [];
+  }
+
+  json[commentKey].push(comment);
+  return json;
+};
+
+exports.read = function (json) {
+  if(json[commentKey] && Array.isArray(json[commentKey])) {
+    return json[commentKey].join("\n");
+  }
+};
+
+exports.strip = function (json) {
+  json = merge(true, json);
+
+  json[commentKey] = undefined;
+
+  return json;
+};
